af8dfba6a17dbc74271cf690fd2a3113dfb59100
jeffrey <yephrey@gmail.com>
2020-12-22 17:56:55 -0500

    index page header start

--- index.php
+++ index.php
@@ -12,6 +12,31 @@ echo "<h1>hello world.</h1>";
 </head>
 
 <body>
+    <div class="container" id="maintainer">
+
+        <header>
+            <code class="funct">getDeveloper<span class="white">(</span><span class="white">"jeffreyMoore"</span><span class="white">)</span> <span class="bracket">{</span></code>
+            <nav>
+                <code class="inner">return(<span class="bracket">{</span></code>
+                <a href="view/who.php">"who"</a><code><span class="white">, </span></code>
+                <a href="#">"what"</a><code><span class="white">, </span></code>
+                <a href="#">"when"</a><code><span class="white">, </span></code>
+                <a href="#">"where"</a><code><span class="white">, </span></code>
+                <a href="#">"why"</a><code><span class="white">, </span></code>
+                <a href="#">"how"</a>
+                <code class="inner"><span class="bracket">}</span>);</code>
+            </nav>
+            <code class="bracket">}</code>
+        </header>
+
+        <main>
+        </main>
+
+        <footer>
+            &copy moorebid jeffrey and the queue cru 2021
+        </footer>
+
+    </div>
 
 </body>
 
