bbb0ecebc11ac4606cff420d7d1e3fbda039c6fa
jeffrey <yephrey@gmail.com>
2020-12-22 19:07:39 -0500

    changed font and font size

--- index.php
+++ index.php
@@ -8,6 +8,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/styles.css">
+    <!-- fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Fira+Code&family=Fira+Mono&display=swap" rel="stylesheet">
     <title>jeffreyMoore | Software Engineer</title>
 </head>
 
