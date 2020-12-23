9aeddbb4a014047a7ac7587c86fb831798d10e7f
jeffrey <yephrey@gmail.com>
2020-12-23 07:52:07 -0500

    implemented header template

---
+++ includes/header.php
@@ -0,0 +1,18 @@
+<header>
+    <div class="container" id="headwrap">
+        <code>
+            <code class="burn">getDeveloper<span class="bone">(</span><span class="bone">"jeffreyMoore"</span><span class="bone">)</span> <span class="bracket">{</span></code>
+            <nav>
+                <code class="bone">return(<span class="bracket">{</span></code>
+                <a href="view/who.php">"who"</a><code><span class="bone">, </span></code>
+                <a href="#">"what"</a><code><span class="bone">, </span></code>
+                <a href="#">"when"</a><code><span class="bone">, </span></code>
+                <a href="#">"where"</a><code><span class="bone">, </span></code>
+                <a href="#">"why"</a><code><span class="bone">, </span></code>
+                <a href="#">"how"</a>
+                <code class="bone"><span class="bracket">}</span>);</code>
+            </nav>
+            <code class="bracket">}</code>
+        </code>
+    </div>
+</header>
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 ?>
 
 <!DOCTYPE html>
@@ -16,21 +17,7 @@
 
 <body>
     <div class="container" id="maintainer">
-
-        <header>
-            <code class="burn">getDeveloper<span class="bone">(</span><span class="bone">"jeffreyMoore"</span><span class="bone">)</span> <span class="bracket">{</span></code>
-            <nav>
-                <code class="bone">return(<span class="bracket">{</span></code>
-                <a href="view/who.php">"who"</a><code><span class="bone">, </span></code>
-                <a href="#">"what"</a><code><span class="bone">, </span></code>
-                <a href="#">"when"</a><code><span class="bone">, </span></code>
-                <a href="#">"where"</a><code><span class="bone">, </span></code>
-                <a href="#">"why"</a><code><span class="bone">, </span></code>
-                <a href="#">"how"</a>
-                <code class="bone"><span class="bracket">}</span>);</code>
-            </nav>
-            <code class="bracket">}</code>
-        </header>
+        <?php include_once 'includes/header.php' ?>
 
         <main>
         </main>
