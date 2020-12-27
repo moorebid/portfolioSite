c60562d06a0cabc987e76b428405f3495954eefb
jeffrey <yephrey@gmail.com>
2020-12-27 10:26:11 -0500

    added all template pages

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+// const heightOutput = document.querySelector("#height");
+// const widthOutput = document.querySelector("#width");
+
+function reportWindowSize() {
+  let heightOutput = window.innerHeight;
+  let widthOutput = window.innerWidth;
+  let height = document.getElementById("height");
+  let width = document.getElementById("width");
+  height.innerText = heightOutput;
+  width.innerText = widthOutput;
+}
+
+window.onresize = reportWindowSize;
---
+++ views/why.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/styles.css">
+    <!-- fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Fira+Code&family=Fira+Mono&display=swap" rel="stylesheet">
+    <title>jeffreyMoore | Software Engineer</title>
+</head>
+
+<body>
+    <div class="container" id="maintainer">
+        <header id="deadHeader">
+            <div class="container" id="headwrap">
+                <code>
+                    <code class="burn">getDeveloper<span class="bone">(</span><span class="bone">"jeffreyMoore"</span><span class="bone">)</span> <span class="bracket">{</span></code>
+                    <nav>
+                        <code class="bone">return(<span class="bracket">{</span></code>
+                        <a href="#">"who"</a><code><span class="bone">, </span></code>
+                        <a href="#">"what"</a><code><span class="bone">, </span></code>
+                        <a href="#">"when"</a><code><span class="bone">, </span></code>
+                        <a href="#">"where"</a><code><span class="bone">, </span></code>
+                        <a href="#">"why"</a><code><span class="bone">, </span></code>
+                        <a href="#">"how"</a>
+                        <code class="bone"><span class="bracket">}</span>);</code>
+                    </nav>
+                    <code class="bracket">}</code>
+                </code>
+            </div>
+        </header>
+        <main>
+        </main>
+
+        <footer>
+            &copy moorebid jeffrey and the queue cru 2021
+        </footer>
+        <script src="js/scripts.js"></script>
+    </div>
+
+    <div class="container" id="windowstuff">
+        <p>Resize the browser window to fire the <code>resize</code> event.</p>
+        <p>Window height: <span id="height"></span></p>
+        <p>Window width: <span id="width"></span></p>
+    </div>
+</body>
+
+</html>
