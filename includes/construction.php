ec0d10b9621ad98f3d70e4a7ca0c266fdd626f0d
jeffrey <yephrey@gmail.com>
2020-12-27 18:49:57 -0500

    awe shoot one moore mod

---
+++ includes/construction.php
@@ -0,0 +1,4 @@
+<p id="construction">
+    I'm working on this portfolio site, sorry for any inconvenience. If you would like to follow along you can at
+    <a href="https://github.com/moorebid/portfolioSite" target="_blank" rel="noopener noreferrer">this git hub repo.</a>
+</p>
