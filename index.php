e9bb9b3a4cf8154fc5793138f4e738d2da9a92c8
jeffrey <yephrey@gmail.com>
2020-12-27 20:10:11 -0500

    moved pages and added site description

--- index.php
+++ index.php
@@ -1,2 +1,36 @@
 <?php
-header("Location: views/home.php");
+// header("Location: views/home.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Jeffrey Moore is a freelance software developer that lives in Wilmington NC.  He can help you with projects ranging from a simple bash script to database driven applications. He is a believer in Agile software development, and a great member of a collaborative team. Java, C, Python, SQL, Kotlin, and PHP are just a few languages that he uses regularly.">
+    <link rel="stylesheet" href="css/styles.css">
+    <!-- fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Fira+Code&family=Fira+Mono&display=swap" rel="stylesheet">
+    <title>jeffreyMoore | Software Engineer</title>
+</head>
+
+<body>
+    <div class="container" id="maintainer">
+        <?php include_once("includes/header.php"); ?>
+
+        <main>
+            <?php include_once("includes/construction.php"); ?>
+        </main>
+
+        <footer>
+            &copy moorebid jeffrey and the queue cru 2021
+        </footer>
+        <script src="/js/scripts.js"></script>
+    </div>
+
+
+</body>
+
+</html>
--- why.php
+++ why.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/styles.css">
+    <meta name="description" content="Jeffrey Moore is a freelance software developer that lives in Wilmington NC.  He can help you with projects ranging from a simple bash script to designing you a database.  Java, C, Python, SQL, Kotlin, and PHP are just a few languages that Jeffrey uses regularly.">
+    <link rel="stylesheet" href="css/styles.css">
     <!-- fonts -->
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Fira+Code&family=Fira+Mono&display=swap" rel="stylesheet">
@@ -13,10 +14,10 @@
 
 <body>
     <div class="container" id="maintainer">
-        <?php include_once("../includes/header.php"); ?>
+        <?php include_once("includes/header.php"); ?>
 
         <main>
-            <?php include_once("../includes/construction.php"); ?>
+            <?php include_once("includes/construction.php"); ?>
         </main>
 
         <footer>
